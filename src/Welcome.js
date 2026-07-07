506564681e004d03bedf4d7dd00110fec205f83e
Afrida khatoon <afridagyash@gmail.com>
2026-07-07 11:33:28 +0530

    xyz

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,4 +1,4 @@
-import axios from 'axios'
+/* import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { baseurl } from './modules/services/Urlpath'
@@ -35,22 +35,67 @@ function Welcome() {
 
         </div>
 
-        /*<div className='contaier-fluid'>
-            <div className='row'>
-        
-                        <div className='col-md-3 mt-3'>
-                            <Link to="/" className='card shadow'>
-                                <div className="card-body text-black text-center" >
-                                    <h5 className='card-tittle'>Icon</h5>
-                                    <p className='card-text'>bvbmnbmh</p>
-                                </div>
-                            </Link>
-                        </div>
-
-            </div>
-
-        </div> */
     )
 }
 
 export default Welcome
+ */
+
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { baseurl } from "./modules/services/Urlpath";
+import "./css/style.css"
+import { FaTachometerAlt, FaCogs, FaCalculator, FaKey, FaChartLine, FaCreditCard, FaBoxOpen, FaUniversity, FaClipboardList, FaIndustry, FaUsers, FaFileAlt } from "react-icons/fa";
+
+function Welcome() {
+    const [myapp, updateapp] = useState([]);
+
+    useEffect(() => {
+        axios.get(`${baseurl}/addlist`).then((res) => {
+            updateapp(res.data.addlist);
+        });
+    }, []);
+    const icons = {
+        users: <FaTachometerAlt size={60} />,
+        administration: <FaCogs size={60} />,
+        finance: <FaCalculator size={60} />,
+        fixedassets: <FaKey size={60} />,
+        sales: <FaChartLine size={60} />,
+        purchase: <FaCreditCard size={60} />,
+        inventory: <FaBoxOpen size={60} />,
+        banking: <FaUniversity size={60} />,
+        planning: <FaClipboardList size={60} />,
+        production: <FaIndustry size={60} />,
+        hr: <FaUsers size={60} />,
+        reports: <FaFileAlt size={60} />
+    };
+
+    return (
+        <div className="dashboard-container">
+
+            {/* Search Box */}
+            <div className="search-box">
+                <input type="text" placeholder="Search here..." />
+            </div>
+
+            {/* Apps */}
+            <div className="app-grid">
+                {myapp.map((m, index) => (
+                    <Link to={m.approute} className="app-card" key={index}>
+                        <div className={`icon-circle ${m.apptheams}`}>
+                            <div className={`icon-circle ${m.apptheams}`}>
+                                {icons[m.approute.toLowerCase()]}
+                            </div>
+                        </div>
+
+                        <h6>{m.appname}</h6>
+                    </Link>
+                ))}
+            </div>
+
+        </div>
+    );
+}
+
+export default Welcome;
